53eaa809159d767d910e56db1499a754fc4b0969
olindos <yason4ik87@gmail.com>
2017-11-08 16:37:05 +0200

    Fix test person_spec

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
+  let(:person) do
+    Person.new(first_name: 'Yason', last_name: 'Mogutin')
+  end
+
+  it 'is invalid without a first_name' do
+    person.first_name = nil
+    expect(person).not_to be_valid
+  end
+
   it 'is invalid without a last name' do
-    person = Person.new(first_name: "Yas", last_name: nil)
+    person.last_name = nil
     expect(person).not_to be_valid
   end
 end
