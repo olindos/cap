cf141c512a5534e25d5e7809273457fa3aa6468c
olindos <yason4ik87@gmail.com>
2017-11-08 16:28:39 +0200

    apply test do people

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without a last name' do
+    person = Person.new(first_name: "Yas", last_name: nil)
+    expect(person).not_to be_valid
+  end
 end
